7657fcdf9d1c314b1cd6c1bcbb228f3ea3bdd679
dorozcoa <davidorozcoarcila@gmail.com>
2020-09-21 21:51:18 -0500

    Se actualizan componentes basicos de angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,44 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material.module';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http'
 
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MatTabsModule} from '@angular/material/tabs';
-import {MatToolbarModule} from '@angular/material/toolbar';
+import { LoginComponent } from './pages/login/login.component';
+import { HomeComponent } from './pages/home/home.component';
+import { RegistroComponent } from './pages/registro/registro.component';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { PokemonCardComponent } from './components/pokemon-card/pokemon-card.component';
+import { FilterPipe } from './pipes/filter.pipe';
+import { FilterTextPipe } from './pipes/filter-text.pipe';
+import { DetalleComponent } from './pages/detalle/detalle.component';
+import { PokemonCardFirebaseComponent } from './components/pokemon-card-firebase/pokemon-card-firebase.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    RegistroComponent,
+    ToolbarComponent,
+    PokemonCardComponent,
+    DetalleComponent,
+    PokemonCardFirebaseComponent,
+    FilterPipe,
+    FilterTextPipe
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatTabsModule,
-    MatToolbarModule
+    MaterialModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/material.module.ts
@@ -0,0 +1,53 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {
+  MatButtonModule, MatCardModule, MatDialogModule, MatInputModule, MatTableModule,
+  MatToolbarModule, MatMenuModule, MatIconModule, MatProgressSpinnerModule, MatTabsModule,
+  MatCheckboxModule, MatBadgeModule, MatDividerModule, MatGridListModule, MatAutocompleteModule,
+  MatTooltipModule
+} from '@angular/material';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatCardModule,
+    MatInputModule,
+    MatDialogModule,
+    MatTableModule,
+    MatMenuModule,
+    MatIconModule,
+    MatProgressSpinnerModule,
+    MatTabsModule,
+    MatCheckboxModule,
+    MatBadgeModule,
+    MatDividerModule,
+    MatGridListModule,
+    MatAutocompleteModule,
+    MatTooltipModule
+  ],
+  exports: [
+    MatToolbarModule,
+    MatButtonModule,
+    MatCardModule,
+    MatInputModule,
+    MatDialogModule,
+    MatTableModule,
+    MatMenuModule,
+    MatIconModule,
+    MatProgressSpinnerModule,
+    MatTabsModule,
+    MatCheckboxModule,
+    MatBadgeModule,
+    MatDividerModule,
+    MatGridListModule,
+    MatAutocompleteModule,
+    MatTooltipModule
+  ]
+})
+
+export class MaterialModule { }

44334bcc59c3e0061908c4fd47f6a1a3a6a7f64a
LuisPaez <paezz2013@gmail.com>
2020-09-23 00:56:51 -0500

    Se agrega el componente de login y la libreria SweetAlert

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,13 @@ import { MaterialModule } from './material.module';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http'
 
+//Pages
+import { LoginComponent } from './pages/login/login.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/pages/login/login.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { LoginModel } from '../../models/login.model';
+
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

dd123ac3dc233239cb30536c59f16456b529b225
dorozcoa <davidorozcoarcila@gmail.com>
2020-10-06 19:43:52 -0500

    Actualizaciond el app-rounting.module y del index

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { LoginComponent } from './pages/login/login.component';
 import { HomeComponent } from './pages/home/home.component';
-import { AuthGuard } from './guards/auth.guard';
 import { RegistroComponent } from './pages/registro/registro.component';
+import { LoginComponent } from './pages/login/login.component';
+import { AuthGuard } from './guards/auth.guard';
+import { DetalleComponent } from './pages/detalle/detalle.component';
 
 const routes: Routes = [
-  { path: 'home', component: HomeComponent, canActivate: [ AuthGuard ] },
-  { path: 'login', component: LoginComponent },
+  { path: 'home', component: HomeComponent, canActivate: [ AuthGuard ]},
+  { path: 'detalle/:id', component: DetalleComponent, canActivate: [ AuthGuard ] },
   { path: 'registro', component: RegistroComponent },
-  { path: '**', component: LoginComponent }
+  { path: 'login', component: LoginComponent },
+  
+  { path: '**', component: LoginComponent },
 ];
 
 @NgModule({
